b399d4425f8f5627486bc3f53bcb924366253a16
Marcin Lukowski <marcin.lukowkski@axbit.com>
2022-10-28 01:09:05 +0200

    .

---
+++ backend/java/src/main/java/com/example/java/JavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(JavaApplication.class, args);
+	}
+
+}
