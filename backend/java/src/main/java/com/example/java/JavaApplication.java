284aef7efc1051bd41033d297246f56ed473ae5f
Marcin Lukowski <marcin.lukowkski@axbit.com>
2022-10-28 00:25:28 +0000

    a

--- backend/java/src/main/java/com/example/java/JavaApplication.java
+++ backend/java/src/main/java/com/example/java/JavaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("tesllssst");
+		System.out.println("marcin");
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
