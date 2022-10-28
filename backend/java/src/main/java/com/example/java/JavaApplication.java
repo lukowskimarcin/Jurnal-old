84f41c948f189ad1ca0a02d89eac550c08b78258
Marcin Lukowski <marcin.lukowkski@axbit.com>
2022-10-28 02:13:59 +0200

    react

--- backend/java/src/main/java/com/example/java/JavaApplication.java
+++ backend/java/src/main/java/com/example/java/JavaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("tesllt");
+		System.out.println("tesllssst");
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
