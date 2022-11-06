eb5f6cc43e3b751ed95ce8d8c83348d510046292
Marcin Lukowski <marcin.lukowkski@axbit.com>
2022-11-06 22:06:46 +0100

    .

---
+++ backend/java/discovery/src/test/java/com/marcinlk/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinlk.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- backend/java/gateway/src/main/java/com/marcinlk/gateway/GatewayApplication.java
+++ backend/java/gateway/src/main/java/com/marcinlk/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.marcinlk.jurnal;
+package com.marcinlk.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class JurnalApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JurnalApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
